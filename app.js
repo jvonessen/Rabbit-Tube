c8c9fdb6c3ff647a88a19d321443afd137c04353
Jen Von Essen <cgajen09@gmail.com>
2017-01-26 13:32:24 -0800

    stylized, fully functional

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function getVideoData(searchTerm, callback) {
       part: 'snippet',
       key: 'AIzaSyBUlJDadHRKtg7aOfHtBUXxfy5-ZRYKX9Y',
       q: searchTerm,
-      maxResults: 10,
+      maxResults: 15,
     };
     $.getJSON(youtube_URL, query, callback)
   } else {
@@ -35,7 +35,7 @@ function getVideoData(searchTerm, callback) {
         part: 'snippet',
         key: 'AIzaSyBUlJDadHRKtg7aOfHtBUXxfy5-ZRYKX9Y',
         q: searchTerm,
-        maxResults: 10,
+        maxResults: 15,
         pageToken: initialPageToken
       };
       $.getJSON(youtube_URL, query, callback)
@@ -48,29 +48,30 @@ function displayVideoData(data) {
     initialPageToken = data.nextPageToken;
     data.items.forEach(function(item) {
       if (item.id.kind == "youtube#video") {
-        result += '<li class="js-search-results-item">' + item.snippet.title +
-        '</br><a href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img src="' +
+        result += '<li class="js-search-results-item col-4">' +
+        '<a href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img src="' +
         item.snippet.thumbnails.medium.url +
-        '" class="img-thumbnail" alt="' + item.snippet.title +
-        '" width="300"></a></li>'
+        '" class="img-thumbnail" alt="' + item.snippet.title + '"></a></br>' + item.snippet.title + '</li>'
       } else if (item.id.kind == "youtube#channel") {
-        result += '<li class="js-search-results-item">' + item.snippet.title +
-        '</br><a href="https://www.youtube.com/channel/' + item.id.channelId + '"><img src="' +
+        result += '<li class="js-search-results-item col-4">' +
+        '<a href="https://www.youtube.com/channel/' + item.id.channelId + '"><img src="' +
         item.snippet.thumbnails.medium.url +
-        '" class="img-thumbnail" alt="' + item.snippet.title +
-        '" width="300"></a></li>'
+        '" class="img-thumbnail" alt="' + item.snippet.title + '"></a></br>' + item.snippet.title + '</li>'
       } else if (item.id.kind == "youtube#playlist") {
-        result += '<li class="js-search-results-item">' + item.snippet.title +
-        '</br><a href="https://www.youtube.com/playlist?list=' + item.id.playlistId + '"><img src="' +
+        result += '<li class="js-search-results-item col-4">' +
+        '<a href="https://www.youtube.com/playlist?list=' + item.id.playlistId + '"><img src="' +
         item.snippet.thumbnails.medium.url +
-        '" class="img-thumbnail" alt="' + item.snippet.title +
-        '" width="300"></a></li>'}
+        '" class="img-thumbnail" alt="' + item.snippet.title + '"></a></br>' + item.snippet.title + '</li>'}
     });
   }
   else {
     result += '<p>No results</p>';
   }
   $('.js-search-results').append('<ul class="js-search-results-page-' + page + '">' + result + '</ul>');
+}
+
+function addPaginationPage(page) {
+  $(".pagination-footer").append('<li class="pagination-link"><a href="#result-page-' + page + '">' + page + '</a></li>');
 }
 
 function addPage(page) {
